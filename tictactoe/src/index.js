b9ac8e8ba906744f67c1817447377227e5418fe0
FabianoQurino <fabianoquirino2011@hotmail.com>
2023-07-13 23:07:48 -0300

    botao start

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -68,6 +68,16 @@ class Game extends React.Component {
     }
     render() {
         const history = this.state.history;
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+                return (
+                    <li>
+                        <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    </li>
+                )
+        })
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
         let status;
@@ -86,7 +96,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
